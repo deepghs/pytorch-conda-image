05fb80e872a19c241a69a259d0debca166c979f5
narugo1992 <narugo992@gmail.com>
2024-12-09 14:04:19 +0800

    dev(narugo): more scripts

--- pyskeb.sh
+++ pyskeb.sh
@@ -1,5 +1,5 @@
 SKEB_DIR=/data/pyskeb
 
-git clone https://narugo1992:${GH_HOME}@github.com/deepghs/pyskeb_backup.git ${SKEB_DIR}
+git clone https://narugo1992:${GH_TOKEN}@github.com/deepghs/pyskeb_backup.git ${SKEB_DIR}
 cd ${SKEB_DIR}
 
